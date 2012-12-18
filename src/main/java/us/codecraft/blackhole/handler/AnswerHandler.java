0af7ec1837239836b78b794ead92033a9260686b
flashsword20 <flashsword20@gmail.com>
2012-12-18 22:49:55 +0800

    Fix a PTR bug: when query like xxx._aa.0.0.1.172.in-addr.arpa., it cut off the meanless prefix of the query.

--- src/main/java/us/codecraft/blackhole/handler/AnswerHandler.java
+++ src/main/java/us/codecraft/blackhole/handler/AnswerHandler.java
@@ -2,6 +2,8 @@ package us.codecraft.blackhole.handler;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
@@ -49,6 +51,19 @@ public class AnswerHandler implements Handler, InitializingBean {
 		answerProviders.add(patternContainer);
 	}
 
+	// b._dns-sd._udp.0.129.37.10.in-addr.arpa.
+	private final Pattern filterPTRPattern = Pattern
+			.compile(".*\\.(\\d+\\.\\d+\\.\\d+\\.\\d+\\.in-addr\\.arpa\\.)");
+
+	private String filterPTRQuery(String query) {
+		Matcher matcher = filterPTRPattern.matcher(query);
+		if (matcher.matches()) {
+			return matcher.group(1);
+		} else {
+			return query;
+		}
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -58,14 +73,16 @@ public class AnswerHandler implements Handler, InitializingBean {
 	@Override
 	public boolean handle(Message request, Message response) {
 		Record question = request.getQuestion();
-		String string = question.getName().toString();
+		String query = question.getName().toString();
+		if (question.getType() == Type.PTR) {
+			query = filterPTRQuery(query);
+		}
 		if (logger.isDebugEnabled()) {
 			logger.debug("query \t" + Type.string(question.getType()) + "\t"
-					+ DClass.string(question.getDClass()) + "\t" + string);
+					+ DClass.string(question.getDClass()) + "\t" + query);
 		}
 		for (AnswerProvider answerProvider : answerProviders) {
-			String answer = answerProvider
-					.getAnswer(string, question.getType());
+			String answer = answerProvider.getAnswer(query, question.getType());
 			if (answer != null) {
 				try {
 					Record record = new RecordBuilder()
@@ -82,7 +99,7 @@ public class AnswerHandler implements Handler, InitializingBean {
 					ServerContext.setHasRecord(true);
 					return true;
 				} catch (Throwable e) {
-					logger.warn("handling exception ", e);
+					logger.warn("handling exception " + e);
 				}
 			}
 		}
