cb5854a9ed2a7897c3dd67f1c5f566d623b67ad8
flashsword20 <flashsword20@gmail.com>
2012-12-19 22:48:38 +0800

    Invite ehcache.Fix a concurrent bug.Opps!

---
+++ src/main/java/us/codecraft/blackhole/utils/RecordUtils.java
@@ -0,0 +1,38 @@
+package us.codecraft.blackhole.utils;
+
+import org.xbill.DNS.Record;
+
+import us.codecraft.blackhole.config.Configure;
+
+/**
+ * @author yihua.huang@dianping.com
+ * @date Dec 19, 2012
+ */
+public class RecordUtils {
+
+	public static long maxTTL(Record[] records) {
+		if (records == null || records.length == 0) {
+			return Configure.DEFAULT_TTL;
+		}
+		long max = 0;
+		for (Record record : records) {
+			if (record.getTTL() > max) {
+				max = record.getTTL();
+			}
+		}
+		return max;
+	}
+
+	public static long minTTL(Record[] records) {
+		if (records == null || records.length == 0) {
+			return Configure.DEFAULT_TTL;
+		}
+		long min = Integer.MAX_VALUE;
+		for (Record record : records) {
+			if (record.getTTL() < min) {
+				min = record.getTTL();
+			}
+		}
+		return min;
+	}
+}
