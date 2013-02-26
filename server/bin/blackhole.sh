50bdb4f5cf63d1f4217e5d9bde62f51a74ab222e
flashsword20 <flashsword20@gmail.com>
2013-02-26 13:19:39 +0800

    fix jar file name

--- server/bin/blackhole.sh
+++ server/bin/blackhole.sh
@@ -10,26 +10,26 @@ case "$1" in
     ;;
   stop)
     echo "Stopping blackhole"
-    java -jar $HOME_DIR/wifesays.jar -cshutdown > /dev/null
+    java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -cshutdown > /dev/null
     ;;
   restart)
     echo "Stopping blackhole..."
-    java -jar $HOME_DIR/wifesays.jar -cshutdown > /dev/null;
+    java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -cshutdown > /dev/null;
     sleep 2;
     echo "Starting blackhole..."
     java -jar $HOME_DIR/blackhole.jar -d"$HOME_DIR">> $HOME_DIR/log &
     ;;
   reload)
     echo "Reloading blackhole"
-    java -jar $HOME_DIR/wifesays.jar -creload > /dev/null
+    java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -creload > /dev/null
     ;;
   zones)
     vi $HOME_DIR/config/zones
-    java -jar $HOME_DIR/wifesays.jar -creload > /dev/null
+    java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -creload > /dev/null
     ;;
   config)
     vi $HOME_DIR/config/blackhole.conf
-    java -jar $HOME_DIR/wifesays.jar -creload > /dev/null
+    java -jar $HOME_DIR/lib/wifesays-1.0.0-alpha.jar -creload > /dev/null
     ;;
   *)
     echo "Usage: $0 {start|stop|reload|zones|config|restart}"
