629aa74bf397a2cbc010c7282d6ba7265e78d90a
flashsword20 <flashsword20@gmail.com>
2013-03-10 16:19:41 +0800

    add zones file change listener

---
+++ server/src/main/java/us/codecraft/blackhole/zones/ZonesFileRefresher.java
@@ -0,0 +1,58 @@
+package us.codecraft.blackhole.zones;
+
+import java.io.File;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import us.codecraft.blackhole.config.Configure;
+import us.codecraft.blackhole.config.ZonesFileLoader;
+
+/**
+ * @author yihua.huang@dianping.com
+ * @date Dec 29, 2012
+ */
+@Component
+public class ZonesFileRefresher implements InitializingBean {
+
+	@Autowired
+	private Configure configure;
+
+	@Autowired
+	private ZonesFileLoader zonesFileLoader;
+
+	private ScheduledExecutorService scheduledExecutorService = Executors
+			.newScheduledThreadPool(1);
+
+	private long lastFileModifiedTime;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	 */
+	public void afterPropertiesSet() throws Exception {
+		File zonesFile = new File(Configure.getZonesFilename());
+		lastFileModifiedTime = zonesFile.lastModified();
+
+		scheduledExecutorService.scheduleWithFixedDelay(new Runnable() {
+
+			public void run() {
+				File zonesFile = new File(Configure.getZonesFilename());
+				// When two files' last modify time not equal, we consider it is
+				// changed.
+				synchronized (this) {
+					if (zonesFile.lastModified() != lastFileModifiedTime) {
+						lastFileModifiedTime = zonesFile.lastModified();
+						zonesFileLoader.reload();
+					}
+				}
+			}
+		}, 500, 500, TimeUnit.MILLISECONDS);
+	}
+}
