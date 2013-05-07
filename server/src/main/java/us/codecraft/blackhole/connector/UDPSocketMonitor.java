f0f4663198aa7e29c8075be5445881193cacaba0
code4craft <code4crafter@gmail.com>
2013-05-07 20:41:55 +0800

    REALLY fix startup bug this time!

--- server/src/main/java/us/codecraft/blackhole/connector/UDPSocketMonitor.java
+++ server/src/main/java/us/codecraft/blackhole/connector/UDPSocketMonitor.java
@@ -15,84 +15,84 @@ import us.codecraft.blackhole.forward.Forwarder;
 /**
  * Authored by EagleDNS<a href="http://www.unlogic.se/projects/eagledns">
  * http://www.unlogic.se/projects/eagledns</a>
- * 
+ *
  * @author yihua.huang@dianping.com
  * @date 2012-12-17
  */
 public class UDPSocketMonitor extends Thread {
 
-	private Logger log = Logger.getLogger(this.getClass());
+    private Logger log = Logger.getLogger(this.getClass());
 
-	private InetAddress addr;
-	private int port;
-	private static final short udpLength = 512;
-	private DatagramSocket socket;
-	@Autowired
-	private QueryProcesser queryProcesser;
-	@Autowired
-	private Forwarder forwarder;
-	@Autowired
-	private Configure configure;
-	@Autowired
-	private ThreadPools threadPools;
+    private InetAddress addr;
+    private int port;
+    private static final short udpLength = 512;
+    private DatagramSocket socket;
+    @Autowired
+    private QueryProcesser queryProcesser;
+    @Autowired
+    private Forwarder forwarder;
+    @Autowired
+    private Configure configure;
+    @Autowired
+    private ThreadPools threadPools;
 
-	public UDPSocketMonitor(String host, int port)  {
-		super();
+    public UDPSocketMonitor(String host, int port) {
+        super();
         try {
             this.addr = Inet4Address.getByName(host);
             this.port = port;
             socket = new DatagramSocket(port, addr);
         } catch (IOException e) {
             System.err.println("Startup fail, 53 port is taken or has no privilege");
-            e.printStackTrace();
+            log.error("Startup fail, 53 port is taken or has no privilege", e);
             System.exit(-1);
         }
 
-		this.setDaemon(true);
-	}
+        this.setDaemon(true);
+    }
 
-	@Override
-	public void run() {
-		ExecutorService executorService = threadPools.getMainProcessExecutor();
-		log.info("Starting UDP socket monitor on address "
-				+ this.getAddressAndPort());
+    @Override
+    public void run() {
+        ExecutorService executorService = threadPools.getMainProcessExecutor();
+        log.info("Starting UDP socket monitor on address "
+                + this.getAddressAndPort());
 
-		while (true) {
-			try {
+        while (true) {
+            try {
 
-				byte[] in = new byte[udpLength];
-				DatagramPacket indp = new DatagramPacket(in, in.length);
-				indp.setLength(in.length);
-				socket.receive(indp);
-				executorService.execute(new UDPConnectionWorker(indp,
-						queryProcesser,
-						new UDPConnectionResponser(socket, indp), forwarder));
-			} catch (SocketException e) {
+                byte[] in = new byte[udpLength];
+                DatagramPacket indp = new DatagramPacket(in, in.length);
+                indp.setLength(in.length);
+                socket.receive(indp);
+                executorService.execute(new UDPConnectionWorker(indp,
+                        queryProcesser,
+                        new UDPConnectionResponser(socket, indp), forwarder));
+            } catch (SocketException e) {
 
-				// This is usally thrown on shutdown
-				log.debug("SocketException thrown from UDP socket on address "
-						+ this.getAddressAndPort() + ", " + e);
-				break;
-			} catch (IOException e) {
+                // This is usally thrown on shutdown
+                log.debug("SocketException thrown from UDP socket on address "
+                        + this.getAddressAndPort() + ", " + e);
+                break;
+            } catch (IOException e) {
 
-				log.info("IOException thrown by UDP socket on address "
-						+ this.getAddressAndPort() + ", " + e);
-			}
-		}
-		log.info("UDP socket monitor on address " + getAddressAndPort()
-				+ " shutdown");
-	}
+                log.info("IOException thrown by UDP socket on address "
+                        + this.getAddressAndPort() + ", " + e);
+            }
+        }
+        log.info("UDP socket monitor on address " + getAddressAndPort()
+                + " shutdown");
+    }
 
-	public void closeSocket() throws IOException {
+    public void closeSocket() throws IOException {
 
-		log.info("Closing TCP socket monitor on address " + getAddressAndPort()
-				+ "...");
+        log.info("Closing TCP socket monitor on address " + getAddressAndPort()
+                + "...");
 
-		this.socket.close();
-	}
+        this.socket.close();
+    }
 
-	public String getAddressAndPort() {
+    public String getAddressAndPort() {
 
-		return addr.getHostAddress() + ":" + port;
-	}
+        return addr.getHostAddress() + ":" + port;
+    }
 }
--- server/src/main/java/us/codecraft/blackhole/forward/MultiUDPReceiver.java
+++ server/src/main/java/us/codecraft/blackhole/forward/MultiUDPReceiver.java
@@ -31,197 +31,202 @@ import us.codecraft.blackhole.concurrent.ThreadPools;
 
 /**
  * Listen on port 40311 using reactor mode.
- * 
+ *
  * @author yihua.huang@dianping.com
  * @date Jan 16, 2013
  */
 @Component
 public class MultiUDPReceiver implements InitializingBean {
 
-	/**
-	 * 
-	 */
-	private static final int dnsPackageLength = 512;
+    /**
+     *
+     */
+    private static final int dnsPackageLength = 512;
 
-	private Map<String, ForwardAnswer> answers = new ConcurrentHashMap<String, ForwardAnswer>();
+    private Map<String, ForwardAnswer> answers = new ConcurrentHashMap<String, ForwardAnswer>();
 
-	private DatagramChannel datagramChannel;
+    private DatagramChannel datagramChannel;
 
-	private final static int PORT_RECEIVE = 40311;
+    private final static int PORT_RECEIVE = 40311;
 
-	private DelayQueue<DelayStringKey> delayRemoveQueue = new DelayQueue<DelayStringKey>();
+    private DelayQueue<DelayStringKey> delayRemoveQueue = new DelayQueue<DelayStringKey>();
 
-	private static class DelayStringKey implements Delayed {
+    private static class DelayStringKey implements Delayed {
 
-		private final String key;
+        private final String key;
 
-		private final long initDelay;
+        private final long initDelay;
 
-		private long startTime;
+        private long startTime;
 
-		/**
-		 * @param key
-		 * @param initDelay
-		 *            in ms
-		 */
-		public DelayStringKey(String key, long initDelay) {
-			this.startTime = System.currentTimeMillis();
-			this.key = key;
-			this.initDelay = initDelay;
-		}
+        /**
+         * @param key
+         * @param initDelay in ms
+         */
+        public DelayStringKey(String key, long initDelay) {
+            this.startTime = System.currentTimeMillis();
+            this.key = key;
+            this.initDelay = initDelay;
+        }
 
-		public String getKey() {
-			return key;
-		}
+        public String getKey() {
+            return key;
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see java.lang.Comparable#compareTo(java.lang.Object)
-		 */
-		@Override
-		public int compareTo(Delayed o) {
-			long delayA = startTime + initDelay - System.currentTimeMillis();
-			long delayB = o.getDelay(TimeUnit.MILLISECONDS);
-			if (delayA > delayB) {
-				return 1;
-			} else if (delayA < delayB) {
-				return -1;
-			} else {
-				return 0;
-			}
-		}
+        /*
+         * (non-Javadoc)
+         *
+         * @see java.lang.Comparable#compareTo(java.lang.Object)
+         */
+        @Override
+        public int compareTo(Delayed o) {
+            long delayA = startTime + initDelay - System.currentTimeMillis();
+            long delayB = o.getDelay(TimeUnit.MILLISECONDS);
+            if (delayA > delayB) {
+                return 1;
+            } else if (delayA < delayB) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * java.util.concurrent.Delayed#getDelay(java.util.concurrent.TimeUnit)
-		 */
-		@Override
-		public long getDelay(TimeUnit unit) {
-			return unit.convert(
-					startTime + initDelay - System.currentTimeMillis(),
-					TimeUnit.MILLISECONDS);
-		}
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * java.util.concurrent.Delayed#getDelay(java.util.concurrent.TimeUnit)
+         */
+        @Override
+        public long getDelay(TimeUnit unit) {
+            return unit.convert(
+                    startTime + initDelay - System.currentTimeMillis(),
+                    TimeUnit.MILLISECONDS);
+        }
 
-	}
+    }
 
-	@Autowired
-	private CacheManager cacheManager;
-	@Autowired
-	private BlackListManager blackListManager;
-	@Autowired
-	private DNSHostsContainer dnsHostsContainer;
+    @Autowired
+    private CacheManager cacheManager;
+    @Autowired
+    private BlackListManager blackListManager;
+    @Autowired
+    private DNSHostsContainer dnsHostsContainer;
 
-	@Autowired
-	private ConnectionTimer connectionTimer;
+    @Autowired
+    private ConnectionTimer connectionTimer;
 
-	@Autowired
-	private SafeHostManager safeBoxService;
-	@Autowired
-	private ThreadPools threadPools;
+    @Autowired
+    private SafeHostManager safeBoxService;
+    @Autowired
+    private ThreadPools threadPools;
 
-	private Logger logger = Logger.getLogger(getClass());
+    private Logger logger = Logger.getLogger(getClass());
 
-	@Autowired
-	private Configure configure;
+    @Autowired
+    private Configure configure;
 
-	/**
-	 * 
-	 */
-	public MultiUDPReceiver() {
-		super();
-	}
+    /**
+     *
+     */
+    public MultiUDPReceiver() {
+        super();
+    }
 
-	private byte[] removeFakeAddress(Message message, byte[] bytes) {
-		Record[] answers = message.getSectionArray(Section.ANSWER);
-		boolean changed = false;
-		for (Record answer : answers) {
-			String address = StringUtils.removeEnd(answer.rdataToString(), ".");
-			if ((answer.getType() == Type.A || answer.getType() == Type.AAAA)
-					&& blackListManager.inBlacklist(address)) {
-				if (!changed) {
-					// copy on write
-					message = (Message) message.clone();
-				}
-				message.removeRecord(answer, Section.ANSWER);
-				changed = true;
-			}
-		}
-		if (message.getQuestion().getType() == Type.A
-				&& (message.getSectionArray(Section.ANSWER) == null || message
-						.getSectionArray(Section.ANSWER).length == 0)
-				&& (message.getSectionArray(Section.ADDITIONAL) == null || message
-						.getSectionArray(Section.ADDITIONAL).length == 0)
-				&& (message.getSectionArray(Section.AUTHORITY) == null || message
-						.getSectionArray(Section.AUTHORITY).length == 0)) {
-			logger.info("remove message " + message.getQuestion());
-			return null;
-		}
-		if (changed) {
-			return message.toWire();
-		}
-		return bytes;
-	}
+    private byte[] removeFakeAddress(Message message, byte[] bytes) {
+        Record[] answers = message.getSectionArray(Section.ANSWER);
+        boolean changed = false;
+        for (Record answer : answers) {
+            String address = StringUtils.removeEnd(answer.rdataToString(), ".");
+            if ((answer.getType() == Type.A || answer.getType() == Type.AAAA)
+                    && blackListManager.inBlacklist(address)) {
+                if (!changed) {
+                    // copy on write
+                    message = (Message) message.clone();
+                }
+                message.removeRecord(answer, Section.ANSWER);
+                changed = true;
+            }
+        }
+        if (message.getQuestion().getType() == Type.A
+                && (message.getSectionArray(Section.ANSWER) == null || message
+                .getSectionArray(Section.ANSWER).length == 0)
+                && (message.getSectionArray(Section.ADDITIONAL) == null || message
+                .getSectionArray(Section.ADDITIONAL).length == 0)
+                && (message.getSectionArray(Section.AUTHORITY) == null || message
+                .getSectionArray(Section.AUTHORITY).length == 0)) {
+            logger.info("remove message " + message.getQuestion());
+            return null;
+        }
+        if (changed) {
+            return message.toWire();
+        }
+        return bytes;
+    }
 
-	private String getKey(Message message) {
-		return message.getHeader().getID() + "_"
-				+ message.getQuestion().getName().toString() + "_"
-				+ message.getQuestion().getType();
-	}
+    private String getKey(Message message) {
+        return message.getHeader().getID() + "_"
+                + message.getQuestion().getName().toString() + "_"
+                + message.getQuestion().getType();
+    }
 
-	public void registerReceiver(Message message, ForwardAnswer forwardAnswer) {
-		answers.put(getKey(message), forwardAnswer);
-	}
+    public void registerReceiver(Message message, ForwardAnswer forwardAnswer) {
+        answers.put(getKey(message), forwardAnswer);
+    }
 
-	public ForwardAnswer getAnswer(Message message) {
-		return answers.get(getKey(message));
-	}
+    public ForwardAnswer getAnswer(Message message) {
+        return answers.get(getKey(message));
+    }
 
-	public void removeAnswer(Message message, long timeOut) {
-		delayRemoveQueue.add(new DelayStringKey(getKey(message), timeOut));
-	}
+    public void removeAnswer(Message message, long timeOut) {
+        delayRemoveQueue.add(new DelayStringKey(getKey(message), timeOut));
+    }
 
-	private void receive() {
-		ExecutorService processExecutors = threadPools.getMainProcessExecutor();
-		final ByteBuffer byteBuffer = ByteBuffer.allocate(512);
-		while (true) {
-			try {
-				byteBuffer.clear();
-				final SocketAddress remoteAddress = datagramChannel
-						.receive(byteBuffer);
-				final byte[] answer = Arrays.copyOfRange(byteBuffer.array(), 0,
-						dnsPackageLength);
-				processExecutors.submit(new Runnable() {
+    private void receive() {
+        ExecutorService processExecutors = threadPools.getMainProcessExecutor();
+        final ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        while (true) {
+            try {
+                byteBuffer.clear();
+                final SocketAddress remoteAddress = datagramChannel
+                        .receive(byteBuffer);
+                final byte[] answer = Arrays.copyOfRange(byteBuffer.array(), 0,
+                        dnsPackageLength);
+                processExecutors.submit(new Runnable() {
 
-					@Override
-					public void run() {
-						try {
-							handleAnswer(answer, remoteAddress);
-						} catch (Throwable e) {
-							logger.warn("forward exception " + e);
-						}
-					}
-				});
+                    @Override
+                    public void run() {
+                        try {
+                            handleAnswer(answer, remoteAddress);
+                        } catch (Throwable e) {
+                            logger.warn("forward exception " + e);
+                        }
+                    }
+                });
 
-			} catch (Throwable e) {
-				logger.warn("receive exception" + e);
-			}
-		}
-	}
+            } catch (Throwable e) {
+                logger.warn("receive exception" + e);
+            }
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
-	 */
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		datagramChannel = DatagramChannel.open();
-		datagramChannel.socket().bind(new InetSocketAddress(PORT_RECEIVE));
-		datagramChannel.configureBlocking(true);
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        datagramChannel = DatagramChannel.open();
+        try {
+            datagramChannel.socket().bind(new InetSocketAddress(PORT_RECEIVE));
+        } catch (IOException e) {
+            System.err.println("Startup fail, maybe another process is running.");
+            logger.error("Startup fail, maybe another process is running.", e);
+            System.exit(-1);
+        }
+        datagramChannel.configureBlocking(true);
         Thread threadForReceive = new Thread(new Runnable() {
 
             @Override
@@ -242,76 +247,76 @@ public class MultiUDPReceiver implements InitializingBean {
         });
         threadForRemove.setDaemon(true);
         threadForRemove.start();
-	}
+    }
 
-	private void removeAnswerFromQueue() {
-		while (true) {
-			try {
-				DelayStringKey delayRemoveKey = delayRemoveQueue.take();
-				answers.remove(delayRemoveKey.getKey());
-				if (logger.isDebugEnabled()) {
-					logger.debug("remove key " + delayRemoveKey.getKey());
-				}
-			} catch (Exception e) {
-				logger.warn("remove answer error", e);
-			}
-		}
-	}
+    private void removeAnswerFromQueue() {
+        while (true) {
+            try {
+                DelayStringKey delayRemoveKey = delayRemoveQueue.take();
+                answers.remove(delayRemoveKey.getKey());
+                if (logger.isDebugEnabled()) {
+                    logger.debug("remove key " + delayRemoveKey.getKey());
+                }
+            } catch (Exception e) {
+                logger.warn("remove answer error", e);
+            }
+        }
+    }
 
-	private void addToBlacklist(Message message) {
-		for (Record answer : message.getSectionArray(Section.ANSWER)) {
-			String address = StringUtils.removeEnd(answer.rdataToString(), ".");
-			if (!blackListManager.inBlacklist(address)) {
-				logger.info("detected dns poisoning, add address " + address
-						+ " to blacklist");
-				blackListManager.addToBlacklist(address);
-			}
-		}
-	}
+    private void addToBlacklist(Message message) {
+        for (Record answer : message.getSectionArray(Section.ANSWER)) {
+            String address = StringUtils.removeEnd(answer.rdataToString(), ".");
+            if (!blackListManager.inBlacklist(address)) {
+                logger.info("detected dns poisoning, add address " + address
+                        + " to blacklist");
+                blackListManager.addToBlacklist(address);
+            }
+        }
+    }
 
-	private void handleAnswer(byte[] answer, SocketAddress remoteAddress)
-			throws IOException {
-		final Message message = new Message(answer);
-		// fake dns server return an answer, it must be dns pollution
-		if (configure.getFakeDnsServer() != null
-				&& remoteAddress.equals(configure.getFakeDnsServer())) {
-			addToBlacklist(message);
-			String domain = StringUtils.removeEnd(message.getQuestion()
-					.getName().toString(), ".");
-			safeBoxService.setPoisoned(domain);
-			return;
-		}
-		if (logger.isTraceEnabled()) {
-			logger.trace("get message from " + remoteAddress + "\n" + message);
-		}
-		final ForwardAnswer forwardAnswer = getAnswer(message);
-		if (forwardAnswer == null) {
-			logger.info("Received messages for "
-					+ message.getQuestion().getName().toString()
-					+ " after timeout!");
-			return;
-		}
-		dnsHostsContainer.registerTimeCost(remoteAddress,
-				System.currentTimeMillis() - forwardAnswer.getStartTime());
-		answer = removeFakeAddress(message, answer);
-		if (logger.isDebugEnabled()) {
-			logger.debug("response message " + message.getHeader().getID()
-					+ " to "
-					+ forwardAnswer.getResponser().getInDataPacket().getPort());
-		}
-		if (answer != null) {
-			forwardAnswer.getResponser().response(answer);
-			cacheManager.setToCache(message, answer);
-		}
-		// connectionTimer.checkConnectTimeForAnswer(forwardAnswer.getQuery(),
-		// message);
+    private void handleAnswer(byte[] answer, SocketAddress remoteAddress)
+            throws IOException {
+        final Message message = new Message(answer);
+        // fake dns server return an answer, it must be dns pollution
+        if (configure.getFakeDnsServer() != null
+                && remoteAddress.equals(configure.getFakeDnsServer())) {
+            addToBlacklist(message);
+            String domain = StringUtils.removeEnd(message.getQuestion()
+                    .getName().toString(), ".");
+            safeBoxService.setPoisoned(domain);
+            return;
+        }
+        if (logger.isTraceEnabled()) {
+            logger.trace("get message from " + remoteAddress + "\n" + message);
+        }
+        final ForwardAnswer forwardAnswer = getAnswer(message);
+        if (forwardAnswer == null) {
+            logger.info("Received messages for "
+                    + message.getQuestion().getName().toString()
+                    + " after timeout!");
+            return;
+        }
+        dnsHostsContainer.registerTimeCost(remoteAddress,
+                System.currentTimeMillis() - forwardAnswer.getStartTime());
+        answer = removeFakeAddress(message, answer);
+        if (logger.isDebugEnabled()) {
+            logger.debug("response message " + message.getHeader().getID()
+                    + " to "
+                    + forwardAnswer.getResponser().getInDataPacket().getPort());
+        }
+        if (answer != null) {
+            forwardAnswer.getResponser().response(answer);
+            cacheManager.setToCache(message, answer);
+        }
+        // connectionTimer.checkConnectTimeForAnswer(forwardAnswer.getQuery(),
+        // message);
 
-	}
+    }
 
-	/**
-	 * @return the datagramChannel
-	 */
-	public DatagramChannel getDatagramChannel() {
-		return datagramChannel;
-	}
+    /**
+     * @return the datagramChannel
+     */
+    public DatagramChannel getDatagramChannel() {
+        return datagramChannel;
+    }
 }
